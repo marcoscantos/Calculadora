828fe2a38606d0df55f019b134b7433f4b4bcaaf
marcoscantos <zx23student3261@colegiovalledelmiro.es>
2024-09-30 08:25:26 +0200

    Suma calculadora.js

---
+++ Ejercicio3/Calculadora.js
@@ -0,0 +1,5 @@
+function sumar(a, b) {
+    return a + b;
+}
+
+console.log("resultado de la suma: ", sumar(2, 3));
---
+++ Ejercicio3/Calculadora.py
@@ -0,0 +1,6 @@
+def sumar(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    resultado = sumar(2, 3)
+    print(f"Resultado de la suma: {resultado}")
