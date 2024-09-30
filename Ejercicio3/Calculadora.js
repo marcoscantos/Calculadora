7313b8ca6f9a7348256e4885c9a05f416ef2c4d6
marcoscantos <zx23student3261@colegiovalledelmiro.es>
2024-09-30 09:05:11 +0200

    Suma calculadora.js

--- Ejercicio3/Calculadora.js
+++ Ejercicio3/Calculadora.js
@@ -3,14 +3,6 @@ document.addEventListener('DOMContentLoaded', function() {
         return a + b;
     }
 
-    function restar(a, b) {
-        return a - b;
-    }
-
-    function multiplicar(a, b) {
-        return a * b;
-    }
-
     window.realizarOperacion = function(operacion) {
         const num1 = parseFloat(document.getElementById('num1').value);
         const num2 = parseFloat(document.getElementById('num2').value);
@@ -23,10 +15,6 @@ document.addEventListener('DOMContentLoaded', function() {
         let resultado;
         if (operacion === 'suma') {
             resultado = sumar(num1, num2);
-        } else if (operacion === 'resta') {
-            resultado = restar(num1, num2);
-        } else if (operacion === 'multiplicacion') {
-            resultado = multiplicar(num1, num2);
         }
 
         document.getElementById('resultado').textContent = "Resultado: " + resultado;
