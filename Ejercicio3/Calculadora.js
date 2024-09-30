d6c5fbdb0eccfca9f9442cacaadd0c5c0108ff34
marcoscantos <zx23student3261@colegiovalledelmiro.es>
2024-09-30 08:28:54 +0200

    Resta calculadora.js

--- Ejercicio3/Calculadora.js
+++ Ejercicio3/Calculadora.js
@@ -2,4 +2,8 @@ function sumar(a, b) {
     return a + b;
 }
 
-console.log("resultado de la suma: ", sumar(2, 3));
+function restar(a, b) {
+    return a - b;
+}
+
+console.log("resultado de la resta: ", restar(5, 3));
