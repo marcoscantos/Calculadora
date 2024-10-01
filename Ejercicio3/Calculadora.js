72b9740c12fa0037c2e9e2a69b01ff6132fa281e
marcoscantos <zx23student3261@colegiovalledelmiro.es>
2024-10-01 13:30:52 +0200

    añadir funcionalidad de dividir

--- Ejercicio3/Calculadora.js
+++ Ejercicio3/Calculadora.js
@@ -1,5 +1,22 @@
-function sumar(a, b) {
-    return a + b;
-}
+function realizarOperacion(operacion) {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    let resultado;
 
-console.log("resultado de la suma: ", sumar(2, 3));
+    if (isNaN(num1) || isNaN(num2)) {
+        resultado = "Por favor ingresa números válidos.";
+    } else {
+        switch (operacion) {
+            case 'suma':
+                resultado = num1 + num2;
+                break;
+            case 'dividir':
+                resultado2 = num2 !== 0 ? num1 / num2 : "No se puede dividir entre cero.";
+                break;
+            default:
+                resultado = "Operación no válida.";
+        }
+    }
+
+    document.getElementById('resultado').innerText = `Resultado: ${resultado}`;
+}
